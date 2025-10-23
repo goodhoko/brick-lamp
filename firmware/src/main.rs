bd9026802711d222cf3c033e3f16494f31c540ff
goodhoko <goodhoko@gmail.com>
2025-10-23 19:03:03 +0200

    Use potentiometer as input, average over multiple samples

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,12 +1,22 @@
 #![no_std]
 #![no_main]
 
+use core::array::from_fn;
+
 use embedded_hal::delay::DelayNs;
 use panic_halt as _;
 
 use attiny_hal::{
-    self as hal, clock,
+    self as hal,
+    adc::{AdcChannel, AdcSettings, ClockDivider, ReferenceVoltage},
+    clock,
+    pac::ADC,
+    port::{
+        mode::{Analog, Io},
+        Pin, PinOps,
+    },
     simple_pwm::{IntoPwmPin, Prescaler, Timer0Pwm},
+    Adc, Attiny,
 };
 use hal::delay::Delay;
 
@@ -22,27 +32,61 @@ fn main() -> ! {
 
     let pwm_timer = Timer0Pwm::new(dp.TC0, Prescaler::Direct);
     let mut pwm = pins.pb0.into_output().into_pwm(&pwm_timer);
-
     pwm.set_duty(0);
     pwm.enable();
 
-    // let max_duty: u8 = (pwm.get_max_duty() as f32 / 100.0 * MAX_POWER_PERCENT) as u8;
+    let mut potentiometer = Potentiometer::new(pins.pb3, dp.ADC);
 
     let mut delay: Delay<CoreClockFrequency> = hal::delay::Delay::new();
 
-    let inc_button = pins.pb3.into_pull_up_input();
-    let dec_button = pins.pb4.into_pull_up_input();
-
-    let mut duty: u8 = 0;
-
     loop {
-        if inc_button.is_low() {
-            duty += 1;
-        } else if dec_button.is_low() {
-            duty -= 1;
-        }
-
-        pwm.set_duty(duty);
+        let input = potentiometer.measure();
+        pwm.set_duty(input);
         delay.delay_ms(10);
+    }
+}
+
+struct Potentiometer<P> {
+    pin: Pin<Analog, P>,
+    adc: Adc<CoreClockFrequency>,
+}
+
+impl<P> Potentiometer<P>
+where
+    P: PinOps,
+    Pin<Analog, P>: AdcChannel<Attiny, ADC>,
+{
+    pub fn new<MODE: Io>(pin: Pin<MODE, P>, adc: ADC) -> Self {
+        let mut adc = Adc::<CoreClockFrequency>::new(
+            adc,
+            AdcSettings {
+                // For best precision the ADC needs between 50 and 200 kHz. Our system clock is 8MHz.
+                // 8 MHZ / 64 = 128 kHz.
+                clock_divider: ClockDivider::Factor64,
+                // The potentiometer ends are wired to GND and 3.3V a.k.a. Vcc.
+                ref_voltage: ReferenceVoltage::AVcc,
+            },
+        );
+
+        let pin = pin.into_analog_input(&mut adc);
+
+        Self { pin, adc }
+    }
+
+    pub fn measure(&mut self) -> u8 {
+        let mut delay: Delay<CoreClockFrequency> = hal::delay::Delay::new();
+        // Measure 5 samples.
+        let mut samples: [u16; 5] = from_fn(|_| {
+            delay.delay_ms(10);
+            self.pin.analog_read(&mut self.adc)
+        });
+
+        samples.as_mut_slice().sort_unstable();
+        // Drop the max and min samples and average the rest
+        let avg = samples[1..4].iter().sum::<u16>() / 3;
+
+        // The ADC has 10 bits of precision but our PWM has only 8bits.
+        // Convert by simply flooring to u8;
+        (avg >> 2) as u8
     }
 }
