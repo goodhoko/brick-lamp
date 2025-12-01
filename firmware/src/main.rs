bd68c6d260bfdda6b851317a65e4acbe667344fb
goodhoko <goodhoko@gmail.com>
2025-12-01 17:56:08 +0100

    Improve legibility of unsafe code

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -29,15 +29,16 @@ fn main() -> ! {
     let mut pwm = pins.pb0.into_output().into_pwm(&pwm_timer);
     pwm.set_duty(invert(0));
 
-    // Change the Compare Output Mode register to the compare_match *inverted* mode to work around
+    // Change the Compare Output Mode register to "compare_match *inverted*" mode to work around
     // a limitation of the "simple PWM" where it can't reach a pure, constant low-level when the
     // duty is set to 0. By inverting the output we can reach constant low-level at duty 255.
     // In exchange, we can't reach a constant high-level but that's OK as we don't want to drive
     // the LEDs at 100% power anyway. This block functionally replaces a call to pwm.enable().
     interrupt::free(|_| {
-        // SAFETY: AFAIK this register is not accessed from interrupt handlers.
-        let register = unsafe { &*avr_device::attiny85::TC0::ptr() };
-        register.tccr0a.modify(|_, w| w.com0a().bits(3));
+        // SAFETY: we are in critical section. This should be fine.
+        let timer_control_register_a = &unsafe { &*avr_device::attiny85::TC0::ptr() }.tccr0a;
+        const SIMPLE_PWM_INVERTED_MODE: u8 = 3;
+        timer_control_register_a.modify(|_, w| w.com0a().bits(SIMPLE_PWM_INVERTED_MODE));
     });
 
     let mut potentiometer = Potentiometer::new(pins.pb3, dp.ADC);
@@ -53,7 +54,7 @@ fn main() -> ! {
     }
 }
 
-// TODO: use precomputed table and gamma of 2.2
+// TODO: use precomputed table and gamma of 2.2 which matches human perception more closely.
 /// Integer-only gamma correction with gamma = 2.0
 pub fn correct_gamma(input: u8) -> u8 {
     let input = input as u16;
